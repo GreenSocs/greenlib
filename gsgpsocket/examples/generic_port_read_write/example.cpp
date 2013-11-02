a2332b26d754e9f42ce2f62981185b07665738ce
Alexis Semren <alexis.semren@greensocs.com>
2013-11-02 01:22:15 +0100

    gsgpsocket: add CMakeLists.txt, delete old Makefile, fix deprecated warning 	    generic_port_read_write
    Signed-off-by: Alexis Semren <alexis.semren@greensocs.com>

--- gsgpsocket/examples/generic_port_read_write/example.cpp
+++ gsgpsocket/examples/generic_port_read_write/example.cpp
@@ -63,10 +63,10 @@ int sc_main(int argc, char* argv[])
 {
 
   /// GreenControl Core instance
-  gs::ctr::GC_Core       core("ControlCore");
+  gs::ctr::GC_Core       core();
   // GreenConfig Plugin
   gs::cnf::ConfigDatabase cnfdatabase("ConfigDatabase");
-  gs::cnf::ConfigPlugin configPlugin("ConfigPlugin", &cnfdatabase);
+  gs::cnf::ConfigPlugin configPlugin(&cnfdatabase);
 
   
   SlaveMem s1("Slave1",3);
