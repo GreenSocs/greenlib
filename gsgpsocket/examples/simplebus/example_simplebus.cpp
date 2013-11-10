06d41df1beb5b6cadf760e7ff42072ccfab50d47
Alexis Semren <alexis.semren@greensocs.com>
2013-11-10 16:01:41 +0100

    gsgpsocket: deprecated warning fixed in example "simplebus"
    Signed-off-by: Alexis Semren <alexis.semren@greensocs.com>

--- gsgpsocket/examples/simplebus/example_simplebus.cpp
+++ gsgpsocket/examples/simplebus/example_simplebus.cpp
@@ -53,10 +53,10 @@
 int sc_main(int, char**)
 {
   /// GreenControl Core instance
-  gs::ctr::GC_Core       core("ControlCore");
+  gs::ctr::GC_Core       core();
   // GreenConfig Plugin
   gs::cnf::ConfigDatabase cnfdatabase("ConfigDatabase");
-  gs::cnf::ConfigPlugin configPlugin("ConfigPlugin", &cnfdatabase);
+  gs::cnf::ConfigPlugin configPlugin(&cnfdatabase);
 	
   sillysort m1("MasterMark",0x4010, "Call me or I will call you");
   sillysort m2("MasterWolle",0x00ff, "I want a faster bus");
